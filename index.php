b289683ea1a09b1774cb231a3473143eded41410
Sean Burke <seantomburke@users.noreply.github.com>
2013-05-28 20:38:21 -0700

    changing spans on output

--- index.php
+++ index.php
@@ -26,45 +26,6 @@
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-
-        <!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html -->
-
-        <!--<div id="navbar" class="navbar navbar-inverse navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="#">RoadTrip</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#contact">Contact</a></li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li class="nav-header">Nav header</li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form class="navbar-form pull-right">
-                            <input class="span2" type="text" placeholder="Email">
-                            <input class="span2" type="password" placeholder="Password">
-                            <button type="submit" class="btn">Sign in</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>-->
         <header class="jumbotron subhead">
         
 
@@ -123,19 +84,19 @@
                 </div>
             </div>
             <div class="row">
-                <div class="span3">
+                <div class="span4">
                     <div class="controls">
                         <span id="gas_span"></span>
                         <h1 id="gas" class=""></h1>
                     </div>
                 </div>
-                <div class="span3">
+                <div class="span4">
                     <div class="controls">
                         <span>Cost per Person:</span>
                         <h1 id="cost" class=""></h1>
                     </div>
                 </div>
-                <div class="span3">
+                <div class="span4">
                     <div class="controls">
                         <span>Total Cost:</span>
                         <h1 id="total" class=""></h1>
@@ -562,19 +523,11 @@
             }
             History.pushState({state:History.getStateId()+1},document.title,search);
         }
-             
-             
+                  
         function updateHistory()
         {
             History.pushState({mpg:mpg, people:people}, 'mpg','?mpg='+mpg+'&people='+people+'&start-location='+start_location+'&end-location='+end_location+'&roundtrip='+roundtrip);
         }
-        
-        
-        
-        
-        
-        
-        
         
         
         </script>
