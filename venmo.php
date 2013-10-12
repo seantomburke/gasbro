2ec3ee98c49059ae1cb4060e2f282ba5c1cef356
Sean Burke <seantomburke@users.noreply.github.com>
2013-10-11 22:39:12 -0700

    venmo

--- venmo.php
+++ venmo.php
@@ -8,12 +8,18 @@ $venmo->me();
 
 ?>
 <html>
+<head>
+<title>GasBro
+</title>
+</head>
 <script type="text/javascript">
+
 function(){
 
-var wind = window.open('http://m.gasbro.com?access_token=<?php echo $_GET["access_token"];?>');
-}
+var wind = window.open('http://m.gasbro.com/?access_token=<?php echo $_GET["access_token"];?>');
+
 self.close();
+}
 </script>
 
 </html>
