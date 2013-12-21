957dd266047bcc046bb631794c1880002e21f2c2
Sean Burke <seantomburke@users.noreply.github.com>
2013-12-21 06:18:25 -0500

    removing annoying alerts

--- index.php
+++ index.php
@@ -61,24 +61,24 @@ $venmo = new Venmo($_GET['access_token']);
       Parse.FacebookUtils.logIn("user_likes,email", {
           success: function(user) {
             if (!user.existed()) {
-              alert("User signed up and logged in through Facebook!");
+              console.log("User signed up and logged in through Facebook!");
             } else {
-              alert("User logged in through Facebook!");
+              console.log("User logged in through Facebook!");
             }
             
             if (!Parse.FacebookUtils.isLinked(user)) {
               Parse.FacebookUtils.link(user, null, {
                 success: function(user) {
-                  alert("Woohoo, user logged in with Facebook!");
+                  console.log("Woohoo, user logged in with Facebook!");
                 },
                 error: function(user, error) {
-                  alert("User cancelled the Facebook login or did not fully authorize.");
+                  console.log("User cancelled the Facebook login or did not fully authorize.");
                 }
               });
             }
           },
           error: function(user, error) {
-            alert("User cancelled the Facebook login or did not fully authorize.");
+            console.log("User cancelled the Facebook login or did not fully authorize.");
           }
         });
         
