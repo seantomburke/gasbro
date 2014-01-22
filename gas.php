3805ef3d671f985a0a56f1036897d076d985be3b
Sean Burke <seantomburke@users.noreply.github.com>
2014-01-22 04:29:06 -0500

    Fixed gas.php for more locations

--- gas.php
+++ gas.php
@@ -1,7 +1,11 @@
 <?php
 
-function get($url)
+error_reporting(1);
+
+function get($lat,$long,$dist,$type,$sort,$key)
 {
+    $url = 'http://api.mygasfeed.com/stations/radius/'.$lat.'/'.$long.'/'.$dist.'/'.$type.'/'.$sort.'/'.$key.'.json';
+
 	$ch = curl_init(); 
 	$headers = array('Content-type: plaintext,');  
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);  
@@ -9,7 +13,31 @@ function get($url)
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$api_response = curl_exec($ch);
-	return $api_response;
+	
+    $json = json_decode($api_response);
+    
+    if($json){
+        //echo '<br>stations:'.count($json->stations);
+            if(count($json->stations) < 1)
+            {
+                if($dist < 20)
+                {
+                    $dist *= 3;
+                    //echo '<br>distance:'.$dist;
+                    return get($lat,$long,$dist,$type,$sort,$key);
+                }
+                else
+                {
+                    $message = "No Gas Stations Within 20 miles";
+                    return json_encode($message);
+                }
+            }
+            else
+            {
+                //echo '<br>returning $json';
+                return json_encode($json);
+            }
+    }
 }
 
 //$api_key = 'rfej9napna';
@@ -19,14 +47,19 @@ $longitude = $_GET['longitude'];
 $latitude = $_GET['latitude']; 
 $distance = ($_GET['distance']) ? $_GET['distance']:'2';
 $gas_index = ($_GET['gas_type']) ? ($_GET['gas_type']):'0';
-$sort_by = ($_GET['sort_by']) ? $_GET['sort_by']:'price';
-
+$sort_by = ($_GET['sort_by']) ? $_GET['sort_by']:'distance';
 $gas_type = array('reg','mid','pre','diesel');
 
-$url = 'http://api.mygasfeed.com/stations/radius/'.$latitude.'/'.$longitude.'/'.$distance.'/'.$gas_type[$gas_index].'/'.$sort_by.'/'.$api_key.'.json';
+if(!$longitude){
+    $message[0] = "No Longitude";
+}
+if(!$latitude){
+    $message[1] = "No Latitude";
+    echo json_encode($message);
+}
 
-echo get($url);
 
+echo get($latitude,$longitude,$distance,$gas_type[$gas_index],$sort_by,$api_key);
 ?>
 
 
